4349aca36b30d5d3f1998625ddc4371a2b02f02b
Michelle Lo <lomichelle42@gmail.com>
2025-01-01 19:09:25 +0800

    IG plots

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from torch import tensor
 import torch as t
 import torch.nn.functional as F
 import numpy as np
-import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 from brackets_datasets import BracketsDataset, SimpleTokenizer
 from model import compute_integrated_gradients_layer
@@ -68,31 +69,31 @@ model = add_perma_hooks_to_mask_pad_tokens(model, tokenizer.PAD_TOKEN)
 
 print("Loaded model")
 
-# N_SAMPLES = 5000
-# with open("brackets_data.json") as f:
-#     data_tuples = json.load(f)
-#     print(f"loaded {len(data_tuples)} examples, using {N_SAMPLES}")
-#     data_tuples = data_tuples[:N_SAMPLES]
+N_SAMPLES = 5000
+with open("brackets_data.json") as f:
+    data_tuples = json.load(f)
+    print(f"loaded {len(data_tuples)} examples, using {N_SAMPLES}")
+    data_tuples = data_tuples[:N_SAMPLES]
 
-# data = BracketsDataset(data_tuples).to(device)
-# data_mini = BracketsDataset(data_tuples[:100]).to(device)
+data = BracketsDataset(data_tuples).to(device)
+data_mini = BracketsDataset(data_tuples[:100]).to(device)
 
-# def run_model_on_data(
-#     model: HookedTransformer, data: BracketsDataset, batch_size: int = 200
-# ):
-#     """Return probability that each example is balanced"""
-#     all_logits = []
-#     for i in range(0, len(data.strs), batch_size):
-#         toks = data.toks[i : i + batch_size]
-#         logits = model(toks)[:, 0]
-#         all_logits.append(logits)
-#     all_logits = t.cat(all_logits)
-#     assert all_logits.shape == (len(data), 2)
-#     return all_logits
+def run_model_on_data(
+    model: HookedTransformer, data: BracketsDataset, batch_size: int = 200
+):
+    """Return probability that each example is balanced"""
+    all_logits = []
+    for i in range(0, len(data.strs), batch_size):
+        toks = data.toks[i : i + batch_size]
+        logits = model(toks)[:, 0]
+        all_logits.append(logits)
+    all_logits = t.cat(all_logits)
+    assert all_logits.shape == (len(data), 2)
+    return all_logits
 
-# test_set = data
-# n_correct = (run_model_on_data(model, test_set).argmax(-1).bool() == test_set.isbal).sum()
-# print(f"\nModel got {n_correct} out of {len(data)} training examples correct!")
+test_set = data
+n_correct = (run_model_on_data(model, test_set).argmax(-1).bool() == test_set.isbal).sum()
+print(f"\nModel got {n_correct} out of {len(data)} training examples correct!")
 
 # applying integrated gradients on model
 
@@ -106,14 +107,19 @@ baseline = input * mask + tokenizer.PAD_TOKEN * (1 - mask)
 
 print(f"Input shape (tokens) {input.shape}")
 
-target_layer = model.embed
-# _, cache_baseline = model.run_with_cache(baseline)
-# layer_baseline = cache_baseline[model.hook_embed.name]
-
-ig = LayerIntegratedGradients(predict, target_layer)
-attributions, approximation_error = ig.attribute(inputs=input,
+ig_embed = LayerIntegratedGradients(predict, model.embed)
+embed_attributions, approximation_error = ig_embed.attribute(inputs=input,
                                                  baselines=baseline,
                                                  return_convergence_delta=True)
 
-print(f"Attributions (shape {attributions.shape}): \n{attributions}")
-print("Error:", approximation_error.item())
+print(f"Attributions (shape {embed_attributions.shape}): \n{embed_attributions}")
+print("\nError:", approximation_error.item())
+
+attrs = embed_attributions[0].numpy()
+plt.imshow(attrs, cmap='viridis')
+plt.colorbar()
+plt.show()
+
+# Attributions for attention head
+
+
